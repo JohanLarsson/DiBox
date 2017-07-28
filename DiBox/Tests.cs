169c47992cfa59471fdaabdaa2c72d9ac28f4a1a
Johan Larsson <johan_gavle1@hotmail.com>
2017-07-28 17:36:59 +0200

    Initial.

---
+++ DiBox/Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace DiBox
+{
+    using Ninject;
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        [Test]
+        public void GetFoo()
+        {
+            var kernel = new StandardKernel();
+            // Ninject wires up the bindings for classes automatically.
+            // Put breakpoints in the ctors of foo and bar and step through the call below.
+            var foo = kernel.Get<Foo>();
+        }
+    }
+
+    public class Foo
+    {
+        public Foo(Bar bar)
+        {
+            this.Bar = bar;
+        }
+
+        public Bar Bar { get; }
+    }
+
+    public class Bar
+    {
+    }
+}
