1298ac9f66ac5f617ed1f2c925a157913350417f
JohanLarsson <johan_gavle1@hotmail.com>
2017-07-30 14:33:25 +0200

    GetExtension

---
+++ WpfApp/GetExtension.cs
@@ -0,0 +1,44 @@
+﻿namespace WpfApp
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Markup;
+
+    using Ninject;
+
+    public class GetExtension : MarkupExtension
+    {
+        private static readonly DependencyObject DependencyObject = new DependencyObject();
+
+        public GetExtension(Type type)
+            : this(type, false)
+        {
+        }
+
+        public GetExtension(Type type, bool isDesignTimeCreatable)
+        {
+            this.Type = type;
+            this.IsDesignTimeCreatable = isDesignTimeCreatable;
+        }
+
+        private static bool IsInDesignMode => DesignerProperties.GetIsInDesignMode(DependencyObject);
+
+        [ConstructorArgument("type")]
+        public Type Type { get; set; }
+
+        [ConstructorArgument("isDesignTimeCreatable")]
+        public bool IsDesignTimeCreatable { get; set; } = false;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (this.Type == null ||
+                (!this.IsDesignTimeCreatable && IsInDesignMode))
+            {
+                return null;
+            }
+
+            return App.Kernel?.Get(this.Type);
+        }
+    }
+}
